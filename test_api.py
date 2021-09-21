93105acb3252d34ee6b2bd9e183ad4f3a7385c3c
Sewenger <Sewengertst@gmail.com>
2021-09-21 22:06:02 +0300

    Added logger

--- test_api.py
+++ test_api.py
@@ -2,6 +2,25 @@ import pytest
 import requests
 from faker import Faker
 import random
+import logging
+
+logger = logging.getLogger('api')
+
+
+class request:
+    @classmethod
+    def post(cls, url, json):
+        logger.info(f'request POST {url} body:{json}')
+        response = requests.post(url, json=json)
+        logger.info(f'response {response.status_code} POST {response.url} body:{response.text}')
+        return response
+
+    @classmethod
+    def get(cls, url):
+        logger.info(f'request POST {url}')
+        response = requests.get(url)
+        logger.info(f'response {response.status_code} POST {response.url} body:{response.text}')
+        return response
 
 
 @pytest.fixture
@@ -16,7 +35,7 @@ def test_create_user(host: str):
         'name': faker.first_name(),
         'job': faker.job()
     }
-    response = requests.post(f'{host}/api/users', json=body)
+    response = request.post(f'{host}/api/users', json=body)
     assert response.status_code == 201
     user = response.json()
 
@@ -35,7 +54,7 @@ def test_create_user_field_name_is_empty(host: str, field: str):
         'job': faker.job()
     }
     del body[field]
-    response = requests.post(f'{host}/api/users', json=body)
+    response = request.post(f'{host}/api/users', json=body)
     assert response.status_code == 400
     error = response.json()
     assert error['msg'] == f'{field.title()} field is required'
@@ -43,17 +62,17 @@ def test_create_user_field_name_is_empty(host: str, field: str):
 
 def test_get_user_by_id(host: str):
     """Получение рандомного пользователья"""
-    users = requests.get(f'{host}/api/users').json()
+    users = request.get(f'{host}/api/users').json()
     random_user = random.choice(users["data"])
-    user = requests.get(f'{host}/api/users/{random_user["id"]}').json()
+    user = request.get(f'{host}/api/users/{random_user["id"]}').json()
     assert user == random_user
 
 
 def test_get_user_by_invalid_id(host: str):
     """Проверка получения несуществующего пользователя"""
-    users = requests.get(f'{host}/api/users').json()
+    users = request.get(f'{host}/api/users').json()
     random_user = max(users["data"], key=lambda x: x["id"])
-    response = requests.get(f'{host}/api/users/{random_user["id"] + 1}')
+    response = request.get(f'{host}/api/users/{random_user["id"] + 1}')
     assert response.status_code == 404
     error = response.json()
     assert error['msg'] == 'User not found'
